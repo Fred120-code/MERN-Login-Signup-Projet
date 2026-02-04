7ddc94ac7b10cb4409ea153bb2ce51060b258fa5
Fred120-code <fredayemtsa@gmail.com>
2026-02-04 16:46:10 +0100

    add func signup and login

--- backend/Controllers/AuthController.js
+++ backend/Controllers/AuthController.js
@@ -9,9 +9,80 @@ const signup = async (req, res) => {
     const user = await UserModel.findOne({ email });
     if (user) {
       return res.status(409).json({
-        message: "User Email already exist",
-        success: false
+        message: "User Email already exist, You can Login",
+        success: false,
       });
     }
-  } catch (error) {}
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const userModel = new UserModel({
+      name,
+      email,
+      password: hashedPassword,
+      plainPassword: password,
+    });
+
+    await userModel.save();
+
+    res.status(201).json({
+      message: "Signup successfully",
+      success: true,
+    });
+  } catch (error) {
+    console.error("Signup Error", error);
+    res.status(500).json({
+      message: "Internal Server Error",
+      success: false,
+    });
+  }
+};
+
+const logiin = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const user = await UserModel.findOne({ email });
+    const errormsg = "Auth Faild: Email Or Password is wrong";
+
+    if (!user) {
+      return res.status(409).json({
+        message: errormsg,
+        success: false,
+      });
+    }
+
+    const isPassEqual = await bcrypt.compare(password, user.password);
+    if (!isPassEqual) {
+      return res.status(409).json({
+        message: errormsg,
+        success: false,
+      });
+    }
+
+    const jwtToken = jwt.sign(
+      { email: user.email, _id: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "24h" },
+    );
+
+    res.status(201).json({
+      message: "Login successfully",
+      success: true,
+      jwtToken,
+      email,
+      name: user.name,
+    });
+  } catch (error) {
+    console.error("Login Error", error);
+    res.status(500).json({
+      message: "Internal Server Error",
+      success: false,
+    });
+  }
+};
+
+module.exports = {
+  signup,
+  logiin,
 };
